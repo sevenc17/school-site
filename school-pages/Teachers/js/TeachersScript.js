7755fcae0cd969d0d8d827f546c96deb4e20c5bd
sevenc17 <103122557+sevenc17@users.noreply.github.com>
2022-10-21 15:25:14 +0300

    Add files via upload

--- school-pages/Teachers/js/TeachersScript.js
+++ school-pages/Teachers/js/TeachersScript.js
@@ -1,42 +1,28 @@
-let mediaQuery1 = window.matchMedia('(max-width: 1350px)');
-let mediaQuery2 = window.matchMedia('(max-width: 768px)');
-let mediaQuery = 0;
+// let mediaQuery1 = window.matchMedia('(max-width: 1350px)');
+// let mediaQuery2 = window.matchMedia('(max-width: 768px)');
+// let mediaQuery = 0;
 
-function media() {
-	
-	if (mediaQuery2.matches) {
-  		let mediaQuery = 2;
-  		return mediaQuery;
-	} else {
-		let mediaQuery = 3;
-		return mediaQuery;
-	}
-}
+// $(document).ready(function(){
+// 	$('.slider').slick({
+// 		dots: true,
+// 		slidesToShow: media(),
+// 		slidesToScroll: 1,
+// 	});
+// });
 
-media()
-
-$(document).ready(function(){
-	$('.slider').slick({
-		dots: true,
-		slidesToShow: media(),
-		slidesToScroll: 1,
-		waitForAnimate: false,
-	});
-});
-
-$(document).ready(function(){
-	$('.home__content-slider').slick({
-		dots: false,
-		arrows: false,
-		autoplay: true,
-		speed: 1000,
-		autoplaySpeed: 1500,
-		pauseOnHover: true,
-		pauseOnFocus: false,
-		slidesToShow: 1,
-		slidesToScroll: 1,
-	});
-});
+// $(document).ready(function(){
+// 	$('.home__content-slider').slick({
+// 		dots: false,
+// 		arrows: false,
+// 		autoplay: true,
+// 		speed: 1000,
+// 		autoplaySpeed: 1500,
+// 		pauseOnHover: true,
+// 		pauseOnFocus: false,
+// 		slidesToShow: 1,
+// 		slidesToScroll: 1,
+// 	});
+// });
 
 const listBurger = document.querySelector('.list-burger');
 const listMenu = document.querySelector('.list-menu');
